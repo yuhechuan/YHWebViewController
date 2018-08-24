f68eb88b9a31a30511084fdc3c431d9510503253
玉河川 <yuhechuan@ruaho.com>
2018-08-24 17:35:53 +0800

    修改

---
+++ sources/YHProxyRouter.h
@@ -0,0 +1,100 @@
+//
+//  YHProxyRouter.h
+//  YHProxyRouterDemo
+//
+//  Created by apple on 2017/11/3.
+//  Copyright © 2017年 玉河川. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YHProxyRouter : NSProxy
+
+
+/**
+ targets array: Class name of sting
+ */
+
+@property (nonatomic, strong,readwrite) NSArray *targets;
+
+/**
+ targetPath:bundle path of .txt file
+ */
+@property (nonatomic, copy,readwrite) NSString *targetPath;
+
+/**
+ Creates and returns a YHProxyRouter object with targets.
+ 
+ @param targets Class name of sting.
+ 
+ @return A new YHProxyRouter .
+ */
++ (instancetype)initWithTargets:(NSArray <NSString *>*)targets;
+
+/**
+ Creates and returns a YHProxyRouter object with targets.
+ 
+ @param targetPath bundle path of .txt file.
+ 
+ @return A new YHProxyRouter .
+ */
++ (instancetype)initWithTargetPath:(NSString *)targetPath;
+
+/**
+ register single target with params
+ @param targetName targetName (NSString *)
+ */
+- (void)registerTargetName:(NSString *)targetName;
+
+/**
+ unregister single target  params
+ @param targetName targetName (NSString *)
+ */
+- (void)unregisterTargetName:(NSString *)targetName;
+
+/**
+ unregister all target without params
+ */
+- (void)unregisterAllTarget;
+
+/**
+  PerformSelector single selector with params
+  @param methodName method name (NSString *)
+ */
+- (id)executeMethod:(NSString *)methodName;
+
+/**
+ PerformSelector multiple selectors with params
+ @param methodNames method name (NSArray <NSString *>*)
+ */
+- (id)executeMethods:(NSArray *)methodNames;
+
+/**
+ PerformSelector single selector has one params
+ @param methodName method name (NSString *)
+ @param param 入参(id)
+ */
+- (id)executeMethod:(NSString *)methodName param:(id)param;
+
+/**
+ PerformSelector single selector has more params
+ @param methodName method name (NSString *)
+ @param params 入参(NSArray<id> *)
+ */
+- (id)executeMethod:(NSString *)methodName params:(NSArray<id> *)params;
+
+/**
+ get methodName List
+ */
+- (NSArray *)methodNameList;
+/**
+  get className From methodName
+ @param methodName method name (NSString *)
+ */
+- (NSString *)methodNameFromMethodName:(NSString *)methodName;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sources/YHProxyRouter.m
@@ -0,0 +1,256 @@
+//
+//  YHProxyRouter.m
+//  YHProxyRouterDemo
+//
+//  Created by apple on 2017/11/3.
+//  Copyright © 2017年 玉河川. All rights reserved.
+//
+
+#import "YHProxyRouter.h"
+#import <objc/runtime.h>
+#import "pthread.h"
+@implementation YHProxyRouter {
+    NSMutableDictionary <NSString *,id>*_classInfosMap;
+    pthread_mutex_t _mutex;
+}
+
++ (instancetype)initWithTargets:(NSArray <NSString *>*)targets {
+    return [[YHProxyRouter alloc]_initWithTargets:targets];
+}
+
+- (instancetype)_initWithTargets:(NSArray <NSString *>*)targets {
+    [self _initializeVariable];
+    [self _registerMethodsWithTargets:targets];
+    return self;
+}
+
++ (instancetype)initWithTargetPath:(NSString *)targetPath {
+    return [[YHProxyRouter alloc]_initWithTargetPath:targetPath];
+}
+
+- (instancetype)_initWithTargetPath:(NSString *)targetPath {
+    [self _initializeVariable];
+    [self _registerMethodsWithTargets:[self _targetsFromTargetPath:targetPath]];
+    return self;
+}
+
+- (void)setTargets:(NSArray *)targets {
+    [self _initializeVariable];
+    [self _registerMethodsWithTargets:targets];
+}
+
+- (void)setTargetPath:(NSString *)targetPath {
+    _targetPath = targetPath;
+    [self _initializeVariable];
+    [self _registerMethodsWithTargets:[self _targetsFromTargetPath:targetPath]];
+}
+
+- (NSArray <NSString *>*)_targetsFromTargetPath:(NSString *)targetPath {
+    NSArray *_type = [targetPath componentsSeparatedByString:@"."];    
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:[_type firstObject] ofType:[_type lastObject]];
+    NSBundle *methodBundle = [NSBundle bundleWithPath:bundlePath];
+    NSError *error;
+    NSArray *methodFiles = [methodBundle pathsForResourcesOfType:@"txt" inDirectory:nil];
+    NSMutableArray *target_names = [NSMutableArray array];
+    for (NSString *methodPath in methodFiles) {
+        NSString *content = [NSString stringWithContentsOfFile:methodPath encoding:NSUTF8StringEncoding error:&error];
+        [target_names addObjectsFromArray:[content componentsSeparatedByString:@"\n"]];
+    }
+    [target_names removeObject:@""];
+    return target_names;
+}
+
+- (void)registerTargetName:(NSString *)targetName {
+    [self _registerMethodsWithTargetName:targetName];
+}
+
+- (void)unregisterTargetName:(NSString *)targetName {
+    [self _unregisterMethodsWithTargetName:targetName];
+}
+
+- (void)unregisterAllTarget {
+    [self _unregisterAllTarget];
+}
+
+- (void)_initializeVariable {
+    _classInfosMap = [NSMutableDictionary dictionary];
+    pthread_mutex_init(&_mutex, NULL);
+}
+
+- (void)_registerMethodsWithTargets:(NSArray <NSString *>*)targets {
+     _targets = targets;
+    [self _unregisterAllTarget];
+    for (NSString *target_name in targets) {
+        [self _registerMethodsWithTargetName:target_name];
+    }
+}
+
+- (void)_registerMethodsWithTargetName:(NSString *)target_name {
+    Class target_class = NSClassFromString(target_name);
+    id target = [[target_class alloc]init];
+    unsigned int numberOfMethods = 0;
+    Method *method_list = class_copyMethodList(target_class, &numberOfMethods);
+    for (int i = 0; i < numberOfMethods; i ++) {
+        [self _registerMethod:method_list[i] target:target];
+    }
+    free(method_list);
+}
+
+- (void)_registerMethod:(Method)temp_method target:(id)target {
+    SEL temp_sel = method_getName(temp_method);
+    const char *temp_method_name = sel_getName(temp_sel);
+    
+    pthread_mutex_lock(&_mutex);
+    [_classInfosMap setObject:target forKey:[NSString stringWithUTF8String:temp_method_name]];
+    pthread_mutex_unlock(&_mutex);
+}
+
+- (void)_unregisterMethodsWithTargetName:(NSString *)target_name {
+    pthread_mutex_lock(&_mutex);
+    [_classInfosMap removeObjectsForKeys:[self _method_list:target_name]];
+    pthread_mutex_unlock(&_mutex);
+}
+
+- (void)_unregisterAllTarget {
+    pthread_mutex_lock(&_mutex);
+    [_classInfosMap removeAllObjects];
+    pthread_mutex_unlock(&_mutex);
+}
+
+- (NSArray <NSString *>*)_method_list:(NSString *)target_name {
+    Class target_class = NSClassFromString(target_name);
+    unsigned int _count = 0;
+    Method *method_list = class_copyMethodList(target_class, &_count);
+    NSMutableArray *methods = [NSMutableArray arrayWithCapacity:_count];
+    for (int i = 0; i < _count; i ++) {
+        SEL temp_sel = method_getName(method_list[i]);
+        const char *temp_method_name = sel_getName(temp_sel);
+        NSString *method_name = [NSString stringWithFormat:@"%s",temp_method_name];
+        [methods addObject:method_name];
+    }
+    free(method_list);
+    return methods;
+}
+
+
+#pragma mark - NSProxy override methods
+- (void)forwardInvocation:(NSInvocation *)invocation{
+    //获取当前选择子
+    SEL sel = invocation.selector;
+    
+    //在字典中查找对应的target
+    id target = [self fitterClass:sel];
+    
+    //检查target
+    if (target && [target respondsToSelector:sel]) {
+        [invocation invokeWithTarget:target];
+    } else {
+        [super forwardInvocation:invocation];
+    }
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)sel{
+    //在字典中查找对应的target
+    id target = [self fitterClass:sel];
+    //检查target
+    if (target && [target respondsToSelector:sel]) {
+        return [target methodSignatureForSelector:sel];
+    } else {
+        NSAssert(0, @"sel is nil or not exist this method or not register method!");
+        return nil;
+    }
+}
+
+- (id)fitterClass:(SEL)sel {
+    //获取选择子方法名
+    NSString *methodName = NSStringFromSelector(sel);
+    pthread_mutex_lock(&_mutex);
+    id target = [_classInfosMap objectForKey:methodName];
+    pthread_mutex_unlock(&_mutex);
+    return target;
+}
+
+- (id)executeMethod:(NSString *)methodName {
+    NSAssert(methodName && methodName.length > 0, @"method name can't be blank");
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    SEL sel = NSSelectorFromString(methodName);
+    return [self performSelector:sel];
+#pragma clang diagnostic pop
+}
+
+- (id)executeMethods:(NSArray *)methodNames {
+    NSMutableDictionary *paramsDict = [NSMutableDictionary dictionary];
+    for (NSString *methodName in methodNames) {
+        id param = [self executeMethod:methodName];
+        if (param) [paramsDict setValue:param forKey:methodName];
+    }
+    return paramsDict.count > 0?paramsDict:nil;
+}
+
+- (id)executeMethod:(NSString *)methodName param:(id)param {
+    NSAssert(methodName && methodName.length > 0, @"method name can't be blank");
+    if (param && ![methodName hasSuffix:@":"]) {
+        methodName = [methodName stringByAppendingString:@":"];
+    }
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    SEL sel = NSSelectorFromString(methodName);
+    return [self performSelector:sel withObject:param];
+#pragma clang diagnostic pop
+}
+
+- (id)executeMethod:(NSString *)methodName params:(NSArray<id> *)params {
+    if (params.count == 0 || !params) {
+        return [self executeMethod:methodName];
+    }
+    NSAssert(methodName && methodName.length > 0, @"method name can't be blanc");
+    if (![methodName hasSuffix:@":"]) {
+        methodName = [methodName stringByAppendingString:@":"];
+    }
+    // 方法签名(方法的描述)
+    SEL sel = NSSelectorFromString(methodName);
+    id target = [self fitterClass:sel];
+
+    NSMethodSignature *signature = [[target class] instanceMethodSignatureForSelector:sel];
+    
+    // NSInvocation : 利用一个NSInvocation对象包装一次方法调用（方法调用者、方法名、方法参数、方法返回值）
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    invocation.target = self;
+    invocation.selector = sel;
+    
+    // 设置参数
+    NSInteger paramsCount = signature.numberOfArguments - 2; // 除self、_cmd以外的参数个数
+    paramsCount = MIN(paramsCount, params.count);
+    for (NSInteger i = 0; i < paramsCount; i++) {
+        id object = params[i];
+        if ([object isKindOfClass:[NSNull class]]) continue;
+        [invocation setArgument:&object atIndex:i + 2];
+    }
+    // 调用方法
+    [invocation invoke];
+    
+    // 获取返回值
+    id returnValue = nil;
+    if (signature.methodReturnLength) { // 有返回值类型，才去获得返回值
+        [invocation getReturnValue:&returnValue];
+    }
+    return returnValue;
+}
+
+- (NSArray *)methodNameList {
+    return [_classInfosMap allKeys];
+}
+
+- (NSString *)methodNameFromMethodName:(NSString *)methodName {
+    id object = _classInfosMap[methodName];
+    Class objectClass = object_getClass(object);
+    return NSStringFromClass(objectClass);
+}
+
+
+- (void)dealloc {
+    pthread_mutex_destroy(&_mutex);
+}
+
+@end
